819d9a909a1e2e2681ed1d83a299237fba9eb481
shinnkura <n.shinzo1010@icloud.com>
2024-12-07 21:26:15 +0900

    リファクタリング

--- atcoder/ex8_takoyaki.cpp
+++ atcoder/ex8_takoyaki.cpp
@@ -23,3 +23,23 @@ int main() {
   cout << price * N << endl;
 }
 
+// 回答
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int p;
+  cin >> p;
+
+  // パターン2
+  if (p == 2) {
+    string text;
+    cin >> text;
+    cout << text << "!" << endl; // これだと、入力直後に出力される。（出力結果的には問題ないが。。）
+  }
+
+  int price, N;
+  cin >> price >> N;
+  cout << price * N << endl;
+}
+
