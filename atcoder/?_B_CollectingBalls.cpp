0d01102d1e879c1e9ab60cc2f7d7cc5c566dd148
shinnkura <n.shinzo1010@icloud.com>
2024-12-09 01:38:09 +0900

    shiftOnly

---
+++ atcoder/?_B_CollectingBalls.cpp
@@ -0,0 +1,1 @@
+// https://atcoder.jp/contests/abc074/tasks/abc074_b
---
+++ atcoder/?_B_PalindromicNumbers.cpp
@@ -0,0 +1,11 @@
+// https://atcoder.jp/contests/abc090/tasks/abc090_b
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+
+
+}
