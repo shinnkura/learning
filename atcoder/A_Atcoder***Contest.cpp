f61a3137ba055aaea33e8ea670fd1bd98940ef52
shinnkura <n.shinzo1010@icloud.com>
2024-12-10 12:17:58 +0900

    A_Atcoder***Contest

---
+++ atcoder/A_Atcoder***Contest.cpp
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc048/tasks/abc048_a
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    cout << "A" << s2.at(0) << "C" << endl;
+}
