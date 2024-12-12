950301686b11cdc5e91fda68cf4e669453c31c05
shinnkura <n.shinzo1010@icloud.com>
2024-12-12 19:51:07 +0900

    stl.cpp

--- atcoder/B_PictureFrame.cpp
+++ atcoder/B_PictureFrame.cpp
@@ -1,7 +1,6 @@
 // https://atcoder.jp/contests/abc062/tasks/abc062_b
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
@@ -9,24 +8,22 @@ int main() {
     cin >> H >> W;
 
     vector<string> vec(H);
-    for (int i = 0; )// で比べる
-    for (int i = 0; i < ; ++i) cin >> vec.at(i);
-
-    // 1行目
-    // 1行目と同じ長さの#を出力
-    // 2行目
-    // Wの最初と最後に#を出力
-    // 最終行目
-    // 1行目と同じ長さの#を出力
     
     for (int i = 0; i < H; ++i) {
-        if (i == 0 || i == H - 1) {
-            for (int j = 0; j < W; ++j) {
-                cout << "#";
-            }
-        } else {
-            cout << "#" << vec.at(i) << "#";
-        }
-        cout << endl;
+        cin >> vec.at(i);
+    }
+
+    for (int i = 0; i < W + 2; ++i) {
+        cout << "#";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < H; ++i) {
+        cout << "#" << vec.at(i) << "#" << endl;
+    }
+
+    for (int i = 0; i < W + 2; ++i) {
+        cout << "#";
     }
 }
