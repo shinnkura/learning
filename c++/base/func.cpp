6381293d7b2290486475b39dc085fb1746d1cd76
shinnkura <n.shinzo1010@icloud.com>
2024-12-20 11:47:36 +0900

    func.cpp追加

--- c++/base/func.cpp
+++ c++/base/func.cpp
@@ -2,9 +2,22 @@
 #include <vector>
 using namespace std;
 
+// 返り値がないときはvoidを書く
+// 返り値があるときは返り値の型を書く
+// 返り値が無いのでvoidを指定
+void hello(string text) {
+  cout << "Hello, " << text << endl;
+  return;
+}
+ 
+int main() {
+  hello("Tom");
+  hello("C++");
+}
 
 // プロトタイプ
 // 関数の宣言だけを行い、中身は後で定義する
+// 普通は関数を宣言した行より後でしか呼び出せない
 void hello(); // プロトタイプ
 
 int main() {
