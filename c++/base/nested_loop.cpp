8ef0c99546b19777cfa489313a6a7548f5b95ab2
shinnkura <n.shinzo1010@icloud.com>
2024-12-20 12:14:36 +0900

    ループの書き方と範囲for文

--- c++/base/nested_loop.cpp
+++ c++/base/nested_loop.cpp
@@ -1,22 +1,26 @@
-// 多重ループを一度に抜けたい場合は、フラグ変数を用意してそれぞれのループを抜けるようにする必要がある
-bool finished = false;  // 外側のループを抜ける条件を満たしているかどうか(フラグ変数)
+#include <iostream>
+using namespace std;
 
-for (int i = 0; i < ... ; i++) {
-for (int j = 0; j < ... ; j++) {
+int main() {
+    bool finished = false;  // 外側のループを抜ける条件を満たしているかどうか(フラグ変数)
 
-    /* 処理 */
+    for (int i = 0; i < 10 ; i++) {
+        for (int j = 0; j < 10 ; j++) {
+            /* 処理 */
+            if (i == 3 && j == 3) { 
+                //  2重ループを抜ける条件
+                finished = true;
+                break;  // (＊)
+                // finishをtrueにしてからbreakすることで、
+                //   内側のループを抜けたすぐ後に外側のループも抜けることができる
+            }
+        }
+        // (＊)のbreakでここに来る
 
-    if (/* 2重ループを抜ける条件 */) {
-    finished = true;
-    break;  // (＊)
-    // finishをtrueにしてからbreakすることで、
-    //   内側のループを抜けたすぐ後に外側のループも抜けることができる
+        if (finished) {
+            break;  // (＊＊)
+        }
     }
-}
-// (＊)のbreakでここに来る
-
-if (finished) {
-    break;  // (＊＊)
-}
+    return 0;
 }
 
