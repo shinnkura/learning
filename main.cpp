c352d15d284e8635ab5b401d45a2d06b64f37558
shinnkura <n.shinzo1010@icloud.com>
2025-01-02 18:38:17 +0900

    stdc++.h

--- main.cpp
+++ main.cpp
@@ -1,42 +1,7 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int count_digit(long long S) {
-    int count = 0;
-    while (S > 0) {
-        S /= 10;
-        count++;
-    }
-    return count;
-}
-
-int find_duble0(long long S) {
-    string s = to_string(S);
-    int count = 0;
-    int size = s.size();
-    int length;
-    if (size != 0) {
-        length = size - 1;
-    } else {
-        length = size;
-    }
-
-    for (int i = 0; i < length; i++) {
-        if (s.at(i) == '0' && s.at(i + 1) == '0') {
-            count++;
-            i++;
-        }
-    }
-    return count;
-}
-
 int main() {
-    long long S;
-    cin >> S;
-    int digit = count_digit(S);
-    int duble0 = find_duble0(S);
-    cout << digit << endl;
-    cout << duble0 << endl;
-    cout << digit - duble0 << endl;
+   cout << "Hello World" << endl;
+   return 0;
 }
