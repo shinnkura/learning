8cf35f475948bbf947fe291dba038a65d7e59778
shinnkura <n.shinzo1010@icloud.com>
2025-01-21 00:07:56 +0900

    abc389

--- atcoder/contest/abc389/b/main.cpp
+++ atcoder/contest/abc389/b/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
+using ll = long long;
 
-int findFactorialNumber(long long input) {
-    long long currentFactorial = 1;
+// １からnまで掛け算して、xと一致するかどうかを調べる
+// 一致したらnを返す
+// xを越えたら、処理を終了して-1を返す
+int findFactorialNumber(ll x) {
+    ll currentFactorial = 1;
     int n = 0;
 
-    while (currentFactorial <= input) {
-        if (currentFactorial == input) {
+    while (currentFactorial <= x) {
+        if (currentFactorial == x) {
             return n;
         }
         ++n;
@@ -17,13 +21,46 @@ int findFactorialNumber(long long input) {
 }
 
 int main() {
-    long long input;
-    cin >> input;
+    ll x;
+    cin >> x;
 
-    int result = findFactorialNumber(input);
+    int result = findFactorialNumber(x);
 
     if (result != -1) {
         cout << result << endl;
     }
     return 0;
+}
+
+
+// ####################
+// 解答例
+// 割り算していって、割り切れるかどうかで判定する
+int main() {
+    ll x;
+    cin >> x;
+    int n = 1;
+    while (x>1) n++, x /=n;
+    cout << n << endl;
+}
+
+
+// ####################
+// 別解
+// 再帰関数
+ll fact(int n) {
+    if (n == 1) return 1;
+    return n * fact(n-1);
+}
+int main() {
+    ll x;
+    cin >> x;
+    int n = 1;
+    while (1) {
+        if (fact(n) == x) {
+            cout << n << endl;
+            return 0;
+        }
+        n++;
+    }
 }
--- atcoder/contest/abc389/c/main.cpp
+++ atcoder/contest/abc389/c/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-typedef long long ll;
+using ll = long long;
 
 int main() {
     ll Q;
@@ -40,4 +40,95 @@ int main() {
         // }
     }
     return 0;
+}
+
+// ######################
+// 解答例
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+int main() {
+    int Q;
+    cin >> Q;
+    vector<ll> l(Q), x(Q);
+    int front = 0, back = 0;
+    ll tx = 0;
+
+    rep(qi, Q) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int L;
+            cin >> L;
+            x[back] = tx; l[back] = L; back++;
+            tx += L;
+        } else if (type == 2) {
+            front++;
+        } else {
+            int k;
+            cin >> k;
+            ll ans = x[front+k] - x[front];
+            cout << ans << endl;
+        }
+    }
+}
+
+// ######################
+// 解答例
+// dequeを使う
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+int main() {
+    int Q;
+    cin >> Q;
+    deque<ll> x;
+    ll tx = 0;
+
+    rep(qi, Q) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int L;
+            cin >> L;
+            x.push_back(tx);
+            tx += L;
+        } else if (type == 2) {
+            x.pop_front();
+        } else {
+            int k;
+            cin >> k;
+            ll ans = x[k] - x[0];
+            cout << ans << endl;
+        }
+    }
+}
+
+// ######################
+// 解答例
+// https://atcoder.jp/contests/abc389/editorial/11975
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+    long long now = 0;
+    vector<long long> x;
+    int id = 0;
+    for (int i = 0; i < q; ++i) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            long long l;
+            cin >> l;
+            x.push_back(now);
+            now += l;
+        } else if (t == 2) {
+            id++;
+        } else {
+            int k;
+            cin >> k;
+            k--;
+            cout << x[id + k] - x[id] << "\n";
+        }
+    }
 }
