8f6a9f54f379c5e28798097e7ae3b678de600dc8
shinnkura <n.shinzo1010@icloud.com>
2025-01-25 20:59:22 +0900

    abc389

--- atcoder/contest/abc389/c/main.cpp
+++ atcoder/contest/abc389/c/main.cpp
@@ -111,14 +111,14 @@ using namespace std;
 int main() {
     int q;
     cin >> q;
-    long long now = 0;
-    vector<long long> x;
+    ll now = 0;
+    vector<ll> x;
     int id = 0;
     for (int i = 0; i < q; ++i) {
         int t;
         cin >> t;
         if (t == 1) {
-            long long l;
+            ll l;
             cin >> l;
             x.push_back(now);
             now += l;
