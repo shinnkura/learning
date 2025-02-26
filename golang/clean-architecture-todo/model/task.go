bb698780d0f7d1fb5ab2db6e73eea06f9c29503b
shinnkura <n.shinzo1010@icloud.com>
2025-02-26 12:27:47 +0900

    migrationエラーの修正

--- golang/clean-architecture-todo/model/task.go
+++ golang/clean-architecture-todo/model/task.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Task struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
 	Title     string    `json:"title" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	User      User      `json:"user" gorm:"foreignKey:UserID; constraint:OnDelete:CASCADE"` // 外部キー連携, 削除時に関連するユーザーも削除
+	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"` // 外部キー連携, 削除時に関連するユーザーも削除
 	UserId    uint      `json:"user_id" gorm:"not null"`
 }
 
