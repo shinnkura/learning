ef92b8f503a0526820367eb1fc93011cb381b990
shinnkura <n.shinzo1010@icloud.com>
2025-03-04 00:02:55 +0900

    main.goを修正

--- golang/clean-architecture-todo/main.go
+++ golang/clean-architecture-todo/main.go
@@ -13,6 +13,11 @@ func main() {
 	userRepository := repository.NewUserRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+
+	taskRepository := repository.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskController := controller.NewTaskController(taskUsecase)
+
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
