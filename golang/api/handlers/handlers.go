8079a53fbfe80ab58fe8c2a197f6e8151ebe87ce
shinnkura <n.shinzo1010@icloud.com>
2025-04-27 15:07:40 +0900

    ２章

--- golang/api/handlers/handlers.go
+++ golang/api/handlers/handlers.go
@@ -1,21 +1,28 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/shinnkura/api/models"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, World!\n")
 }
 
-func PostArticleHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting article!\n")
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
 }
 
 func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +39,9 @@ func ArticleListHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		page = 1
 	}
-
-	resString := fmt.Sprintf("Article list (page: %d)\n", page)
-	io.WriteString(w, resString)
+	log.Println(page)
+	articleList := []models.Article{models.Article1, models.Article2}
+	json.NewEncoder(w).Encode(articleList)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,13 +50,27 @@ func ArticleDetailHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid article ID", http.StatusBadRequest)
 		return
 	}
-	io.WriteString(w, fmt.Sprintf("Article detail! %d\n", articleID))
+	log.Println(articleID)
+	article := models.Article1
+	json.NewEncoder(w).Encode(article)
 }
 
-func PostNiceHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting nice!\n")
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+	article := reqArticle
+	json.NewEncoder(w).Encode(article)
 }
 
-func PostCommentHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Posting comment!\n")
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+	comment := reqComment
+	json.NewEncoder(w).Encode(comment)
 }
